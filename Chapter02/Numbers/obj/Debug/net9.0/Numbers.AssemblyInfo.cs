15425b836f6805d913264d8b0155e11d7711d841
ohhyeun kwon <kdhgus92@gmail.com>
2025-03-08 22:00:43 +0900

    assembly change

--- Chapter02/Numbers/obj/Debug/net9.0/Numbers.AssemblyInfo.cs
+++ Chapter02/Numbers/obj/Debug/net9.0/Numbers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Numbers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb8a0b12c4d1826e5b11852c2eba908ca849e471")]
 [assembly: System.Reflection.AssemblyProductAttribute("Numbers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Numbers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
